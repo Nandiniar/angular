bd9c30b966f99fa0c0316edc47b4d9fd89b31f57
nandini <nandani@contaque.com>
2025-04-29 09:51:16 +0530

    Changes are added
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Nandini';
+  title :string= 'Nandini';
+  name='Arora';
+  // hum function ke andar variablles ko let, var , const le sakte  hai but directly nhi
+  hello(){
+    const x=9;
+  }
+
+  hii(){
+    let y=0;
+  }
+  namaste(){
+    const  z=12;
+  }
+//  const a=90; hum aise nhi likh sakte
+ b:string="hello";
+ x=10;
+ y=20;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// yeh  phale .ts file hai project ke
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
