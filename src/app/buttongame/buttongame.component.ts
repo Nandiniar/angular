81d86e63176742549e04262ecdc573c711818f65
nandini <nandani@contaque.com>
2025-04-29 12:45:01 +0530

    Changes are added
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/buttongame/buttongame.component.ts
+++ src/app/buttongame/buttongame.component.ts
@@ -7,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './buttongame.component.css'
 })
 export class ButtongameComponent {
+   name:string="hello world"
+
+   // agar mutiple  datatype chaiye ek variable ka toh hum 
+    date:string|number="Hello";
+    other:any=123; // any means ke ab datatype koi sa bhi ho sakta hai variable ka
+
+
+  printf(){
+    this.date=12;
+    console.log(this.date);
+  }
+
+   
 solve(){
+  this.other="Nandini"
   alert('hii')
 }
 event(){
@@ -16,5 +30,13 @@ event(){
 }
 otherfunction(){
   console.log("other function")
+}
+
+updateVar(){
+  let x:number=30; // hum aise yeh let function ke andar kar sakte hai but function ke bar nhi
+  console.log(x)
+}
+sum(a:number,b:number){
+  console.log(a+b);
 }
 }
